8ddfb52c141625c85470371f1d9f507138be7788
Haruhi <64474511+winkle777@users.noreply.github.com>
2025-11-21 19:01:06 +0800

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
         'dark': "#333333"
       },
       backgroundImage: { // 添加自定义背景图片
-        'custom-hero': "url('https://blog-image.haruhi.qzz.io/images/G5qse7RacAAAHBL.jpg')",
+        'custom-hero': "url('https://blog-image.haruhi.qzz.io/images/112350855.png')",
         // 你可以在这里添加更多自定义背景
       },
       transitionProperty: {
