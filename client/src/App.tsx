922b35561b7f3042a4b38ab70ef24861dc779c4f
Haruhi <64474511+winkle777@users.noreply.github.com>
2025-11-21 18:21:16 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -198,7 +198,9 @@ function RouteMe({ path, children, headerComponent, paddingClassName, requirePer
   return (
     <Route path={path} >
       {params => {
-        return (<>
+        return (
+        // 关键修改：添加了这个 div 并应用了类名
+        <div className="bg-custom-hero bg-cover bg-center w-full min-h-screen">
           <Header>
             {headerComponent}
           </Header>
